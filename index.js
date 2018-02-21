19f4720a5b94a6ed901a873b8e80c17400f66b74
Joakim Gregersen Quach <joakimquach@gmail.com>
2018-02-21 14:28:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { ifIphoneX } from 'react-native-iphone-x-helper';
 
 export function getStatusBarHeight(skipAndroid: boolean = false) {
     if (Platform.OS === 'ios') {
-        return ifIphoneX(44, 20);
+        return ifIphoneX(30, 20);
     }
 
     if (skipAndroid) {
